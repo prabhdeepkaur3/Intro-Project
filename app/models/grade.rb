6691b5a263e129750a68cb9d3dd9161aa5a15f77
Prabhdeep Kaur <prabhdeepkaur3@academic.rrc.ca>
2024-03-15 01:20:57 -0500

    Made changes in teacher model

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -5,5 +5,5 @@ class Grade < ApplicationRecord
   # Validations
   validates :grade, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
   validates :student_id, presence: true
-  validates :course_id., presence: true
+  validates :course_id, presence: true
 end
